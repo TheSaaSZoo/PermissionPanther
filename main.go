12c2037a931d3289e433ec31eafba86407ef3c47
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-02-01 07:51:12 -0500

    add temp proto files

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/danthegoodman1/PermissionPanther/scylla"
+import (
+	"github.com/danthegoodman1/PermissionPanther/logger"
+	"github.com/danthegoodman1/PermissionPanther/scylla"
+)
 
 func main() {
+	logger.ConfigureLogger()
 	scylla.DBConfig()
 	scylla.DBConnectWithKeyspace()
 }
