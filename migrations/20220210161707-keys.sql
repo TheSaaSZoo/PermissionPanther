2f68f0710084dbb254cfa7d4ad13a3b896993a27
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-02-12 15:40:00 -0500

    update migration

--- migrations/20220210161707-keys.sql
+++ migrations/20220210161707-keys.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS keys (
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   PRIMARY KEY (id)
 );
+CREATE INDEX IF NOT EXISTS keys_by_ns ON keys(ns);
 
 -- +migrate Down
+DROP INDEX IF EXISTS keys_by_ns;
 DROP TABLE IF EXISTS keys;
