8f63d30dee635f471bf256e12e4d0973059e8bb4
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-04-02 09:40:49 -0400

    update hom epage

--- docs_web/src/components/HomepageFeatures.js
+++ docs_web/src/components/HomepageFeatures.js
@@ -35,9 +35,9 @@ const FeatureList = [
 function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
+      {/* <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
-      </div>
+      </div> */}
       <div className="text--center padding-horiz--md">
         <h3 className={clsx(styles.feature__title)}>{title}</h3>
         <p>{description}</p>
